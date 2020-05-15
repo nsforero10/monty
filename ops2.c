479a3e8af07c553a59f1eb18ddcd9e02dad9c041
Nicolas Forero <ns.forero10@gmail.com>
2020-05-14 22:26:42 -0500

    [Feature] Adding task 6

--- ops2.c
+++ ops2.c
@@ -45,7 +45,7 @@ void sub(stack_t **stack, unsigned int line_number)
 
 	(void) line_number;
 	if (!*stack || !crnt->next)
-		fprintf(stderr, "L%u: can't add, stack too short\n", line_number),
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number),
 			frees(), exit(EXIT_FAILURE);
 	while (crnt->next)
 		crnt = crnt->next;
