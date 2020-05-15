043ea0a66288055a9ac40e3a4d5025d4d39b07e3
Nicolas Forero <ns.forero10@gmail.com>
2020-05-14 23:00:13 -0500

    [Feature] Adding task 7

--- ops2.c
+++ ops2.c
@@ -66,12 +66,12 @@ void divide(stack_t **stack, unsigned int line_number)
 
 	(void) line_number;
 	if (!*stack || !crnt->next)
-		fprintf(stderr, "L%u: can't divide, stack too short\n", line_number),
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number),
 			frees(), exit(EXIT_FAILURE);
 	while (crnt->next)
 		crnt = crnt->next;
 	if (crnt->n == 0)
-		fprintf(stderr, "L%u: divideision by zero\n", line_number),
+		fprintf(stderr, "L%u: division by zero\n", line_number),
 		frees(), exit(EXIT_FAILURE);
 	crnt->prev->n /= crnt->n;
 	crnt->prev->next = NULL;
